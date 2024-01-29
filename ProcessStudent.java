d5a38e8c40740e7f9b94fc3d0b9b8a768c2ac37c
Angus Friel <angus.friel.1986@gmail.com>
2024-01-29 12:59:07 +0000

    first commit

---
+++ ProcessStudent.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ProcessStudent{
+    private static final Scanner scan = new Scanner(System.in);
+    private static final String STUDENT_FILE = "students.txt";
+    private static final String STATUS_FILE = "status.txt";
+
+    public static void main(String[] args) throws Exception{
+        while(true){
+            MenuOptions.displayMenu();
+            int choice = getUserChoice();
+
+            switch(choice){
+                case    MenuOptions.STANDARD_OPERATION:
+                        ProcessStudentData(STUDENT_FILE);
+                        break;
+                case    MenuOptions.ADD_DATA:
+                        addDataViaConsole();
+                        break;
+                case    MenuOptions.EXIT:
+                        System.out.println("Goodbye!");
+                        System.exit(0);
+                default:
+                        System.out.println("Invalid choice");        
+            }
+        }
+    }
+
+}
