eba180e3277c806aad97241596649ffc5a6298d5
Angus Friel <angus.friel.1986@gmail.com>
2024-01-29 13:48:17 +0000

    Create new class for students

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String firstName;
+    private String secondName;
+    private int numClasses;
+    private String studentNumber;
+
+    privtae Student(String firstName, String secondName, int numClasses, String studentNumber){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.numClasses = numClasses;
+        this.studentNumber = studentNumber;
+    }
+
+    
+}
