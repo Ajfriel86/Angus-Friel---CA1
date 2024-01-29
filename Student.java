e9a81bf14e6604868b2f469e32e32dcecbf3725d
Angus Friel <angus.friel.1986@gmail.com>
2024-01-29 13:59:26 +0000

    Create method to Parse a string of data to create a Student object

--- Student.java
+++ Student.java
@@ -1,15 +1,43 @@
 public class Student {
+    // Add attributes for the class
     private String firstName;
     private String secondName;
     private int numClasses;
     private String studentNumber;
 
-    privtae Student(String firstName, String secondName, int numClasses, String studentNumber){
+    // Constructor to create the Student Object
+    public Student(String firstName, String secondName, int numClasses, String studentNumber){
         this.firstName = firstName;
         this.secondName = secondName;
         this.numClasses = numClasses;
         this.studentNumber = studentNumber;
     }
 
-    
+    // Parse a string of data to create a Student object
+    public static Student parseStudent(String data){
+        
+        // Split the input data into parts using space as a delimiter
+        String[] parts = data.split(" ");
+        // Check if the data contains exactly three parts
+        if (parts.length == 3){
+            // Extracting individual parts
+            String firstName = parts[0];
+            String secondName = parts[1];
+            int numClasses;
+            // Parse the number of classes as an integer
+            try{
+                numClasses = Integer.parseInt(parts[2]);
+            }
+            // Catch any invalid data
+            catch (NumberFormatException e){
+                return null;
+            }
+            // check validation of individual parts and create a Student object if valid
+            if (isValidFirstName(firstName) && isValidSecondName(secondName) && isValidNumClasses(numClasses)){
+                return new Student(firstName, secondName, numClasses, parts[2]); 
+            } 
+        }
+        return null;
+    }
+
 }
